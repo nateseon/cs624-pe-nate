2203bd86a7299de3e80020fe6663d01ef4a9e21d
Seungwon Seon <165962078+nateseon@users.noreply.github.com>
2025-04-30 01:51:31 +0000

    Update Part1

---
+++ PE03-Todos/todos/app/(tabs)/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import Heading from './Heading';
+import Input from './Input';
+import Button from './Button';
+import TodoList from './TodoList';
+
+let todoIndex = 0;
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: '',
+            todos: [],
+            type: 'All'
+        };
+        this.submitTodo = this.submitTodo.bind(this);
+        this.toggleComplete = this.toggleComplete.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.inputChange = this.inputChange.bind(this);
+    }
+
+    deleteTodo(todoIndex) {
+        let { todos } = this.state;
+        todos = todos.filter((todo) => todo.todoIndex !== todoIndex);
+        this.setState({ todos });
+    }
+
+    toggleComplete(todoIndex) {
+        let todos = this.state.todos;
+        todos.forEach((todo) => {
+            if (todo.todoIndex === todoIndex) {
+                todo.complete = !todo.complete;
+            }
+        });
+        this.setState({ todos });
+    }
+
+    inputChange(inputValue) {
+        console.log('Input Value: ', inputValue);
+        this.setState({ inputValue });
+    }
+
+    submitTodo() {
+        if (this.state.inputValue.match(/^\s*$/)) {
+            return;
+        }
+        const todo = {
+            title: this.state.inputValue,
+            todoIndex,
+            complete: false
+        };
+        todoIndex++;
+        this.setState(prevState => ({
+            todos: [...prevState.todos, todo],
+            inputValue: ''
+        }));
+    }
+
+    render() {
+        const { inputValue, todos} = this.state;
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                  keyboardShouldPersistTaps='always' 
+                  style={styles.content}>
+                    <Heading />
+                    <Input 
+                        inputValue={inputValue}
+                        inputChange={(text) => this.inputChange(text)}
+                    />
+                    <TodoList 
+                        toggleComplete={this.toggleComplete}
+                        deleteTodo={this.deleteTodo}
+                        todos={todos}
+                    />
+                    <Button submitTodo={this.submitTodo} />
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5'
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    }
+});
+
+export default App;
---
+++ PE03-Todos/todos/app/(tabs)/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './App.js';
+
+export default function HomeScreen() {
+  return( 
+    <App />
+  );
+}
