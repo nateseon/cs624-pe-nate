cf6b0db01f923c4f489919eddf1237a261137082
Seungwon Seon <165962078+nateseon@users.noreply.github.com>
2025-04-30 02:26:19 +0000

    PE03 part1 updated

--- PE03-Todos/todos/app/(tabs)/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from './App.js';
-
-export default function HomeScreen() {
-  return( 
-    <App />
-  );
-}
--- pe03/todos/app/(tabs)/App.js
+++ pe03/todos/app/(tabs)/App.js
@@ -1,74 +1,96 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import Heading from './Heading';
 import Input from './Input';
-import Button from './Button'; // import Button component
+import Button from './Button';
+import TodoList from './TodoList';
 
-let todoIndex = 0; // manage todolist
+let todoIndex = 0;
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      inputValue: '',
-      todos: [],
-      type: 'All',
-    };
-    this.submitTodo = this.submitTodo.bind(this);
-  }
-
-  inputChange(inputValue) {
-    console.log('Input Value: ', inputValue);
-    this.setState({ inputValue });
-  }
-
-  submitTodo() {
-    if (this.state.inputValue.match(/^\s*$/)) {
-      return; // empty space not added
+    constructor() {
+        super();
+        this.state = {
+            inputValue: '',
+            todos: [],
+            type: 'All'
+        };
+        this.submitTodo = this.submitTodo.bind(this);
+        this.toggleComplete = this.toggleComplete.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.inputChange = this.inputChange.bind(this);
     }
 
-    const todo = {
-      title: this.state.inputValue,
-      todoIndex,
-      complete: false,
-    };
-    todoIndex++;
+    deleteTodo(todoIndex) {
+        let { todos } = this.state;
+        todos = todos.filter((todo) => todo.todoIndex !== todoIndex);
+        this.setState({ todos });
+    }
 
-    const todos = [...this.state.todos, todo];
-    this.setState({
-      todos,
-      inputValue: '', // empty input after adding
-    }, () => {
-      console.log('State: ', this.state);
-    });
-  }
+    toggleComplete(todoIndex) {
+        let todos = this.state.todos;
+        todos.forEach((todo) => {
+            if (todo.todoIndex === todoIndex) {
+                todo.complete = !todo.complete;
+            }
+        });
+        this.setState({ todos });
+    }
 
-  render() {
-    const { inputValue } = this.state;
-    return (
-      <View style={styles.container}>
-        <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
-          <Heading />
-          <Input
-            inputValue={inputValue}
-            inputChange={text => this.inputChange(text)}
-          />
-          <Button submitTodo={this.submitTodo} />
-        </ScrollView>
-      </View>
-    );
-  }
+    inputChange(inputValue) {
+        console.log('Input Value: ', inputValue);
+        this.setState({ inputValue });
+    }
+
+    submitTodo() {
+        if (this.state.inputValue.match(/^\s*$/)) {
+            return;
+        }
+        const todo = {
+            title: this.state.inputValue,
+            todoIndex,
+            complete: false
+        };
+        todoIndex++;
+        this.setState(prevState => ({
+            todos: [...prevState.todos, todo],
+            inputValue: ''
+        }));
+    }
+
+    render() {
+        const { inputValue, todos} = this.state;
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                  keyboardShouldPersistTaps='always' 
+                  style={styles.content}>
+                    <Heading />
+                    <Input 
+                        inputValue={inputValue}
+                        inputChange={(text) => this.inputChange(text)}
+                    />
+                    <TodoList 
+                        toggleComplete={this.toggleComplete}
+                        deleteTodo={this.deleteTodo}
+                        todos={todos}
+                    />
+                    <Button submitTodo={this.submitTodo} />
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-  content: {
-    flex: 1,
-    paddingTop: 60,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5'
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    }
 });
 
 export default App;
